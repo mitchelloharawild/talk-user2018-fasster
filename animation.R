338599ad3dc4772f452dc041348b2a0140fc3bc2
Mitchell <mitch.ohara-wild@monash.edu>
2018-07-10 23:23:28 +1000

    Added zoom animation

--- animation.R
+++ animation.R
@@ -31,3 +31,37 @@ animate(p, device = "png", width = 1000, height = 600)
 
 frame_vars()$frame_source %>% dirname %>% unique %>% list.files
 
+tsibbledata::elecdemand %>%
+  mutate(state = 1) %>%
+  bind_rows(
+    tsibbledata::elecdemand %>%
+      filter(month(index) == 6) %>% 
+      mutate(state = 2)
+  ) %>% 
+  as_tibble %>% 
+  mutate(index = as.numeric(index)) %>% 
+  ggplot(aes(x=index, y=Demand)) + 
+  geom_line() + 
+  transition_states(state, 8, 8, wrap = FALSE) + 
+  ease_aes('cubic-out') + 
+  view_follow(fixed_x = FALSE)
+  
+yrange <- c(tsibbledata::elecdemand %>%
+  .$Demand %>% range,
+  tsibbledata::elecdemand %>%
+  filter(month(index) == 6) %>%
+  .$Demand %>% range
+)
+
+xrange <- c(tsibbledata::elecdemand %>%
+              .$index %>% range,
+            tsibbledata::elecdemand %>%
+              filter(month(index) == 6) %>%
+              .$index %>% range
+)
+
+p2 <- tsibbledata::elecdemand %>% 
+  autoplot(Demand) + 
+  view_step_manual(1,1, xrange[c(1,3)], xrange[c(2,4)], yrange[c(1,3)], yrange[c(2,4)])
+
+animate(p2, device = "png", width = 1000, height = 600)
