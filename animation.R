d711b50055196c815e0d03491084d50b21c64e6f
Mitchell <mitch.ohara-wild@monash.edu>
2018-07-11 16:49:52 +1000

    Animation updates

--- animation.R
+++ animation.R
@@ -5,6 +5,15 @@ library(tsibble)
 library(ggplot2)
 library(gganimate)
 
+
+gif2mp4 <- function(gif, path){
+  tmp_gif <- tempfile()
+  magick::image_write(gif, tmp_gif)
+  message("Converting gif to mp4...")
+  system(paste0('ffmpeg -i ', tmp_gif, ' -y -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ', path))
+  message("Done!")
+}
+
 anim_points <- c("month", "day", "30 minutes")
 
 make_frame <- function(unit){
@@ -23,13 +32,16 @@ p <- plot_data %>%
   ggplot(aes(x=x, y=Demand)) + 
   geom_line() + 
   xlab("Time") + ylab("Electiricty Demand (GW)") +
-  transition_states(unit, 8, 8, wrap = FALSE) + 
+  transition_states(unit, 10, 1, wrap = FALSE) + 
   ease_aes('cubic-out') + 
   view_follow(fixed_x = TRUE)
 
-animate(p, device = "png", width = 1000, height = 600)
+gif1 <- animate(p, device = "png", width = 1000, height = 600)
 
-frame_vars()$frame_source %>% dirname %>% unique %>% list.files
+gif1_split <- split_animation(gif1, cut(gganimate::frame_vars(gif1)$frame, c(0, 3, 51, 100)))
+
+gif1 %>% 
+  gif2mp4("/home/mitchell/Desktop/test.mp4") %>%
 
 tsibbledata::elecdemand %>%
   mutate(state = 1) %>%
@@ -62,6 +74,14 @@ xrange <- c(tsibbledata::elecdemand %>%
 
 p2 <- tsibbledata::elecdemand %>% 
   autoplot(Demand) + 
-  view_step_manual(1,1, xrange[c(1,3)], xrange[c(2,4)], yrange[c(1,3)], yrange[c(2,4)])
+  xlab("Time") + ylab("Electiricty Demand (GW)") +
+  view_step_manual(1,10, 
+                   c(xrange[1],xrange[c(1,3)]), 
+                   c(xrange[2],xrange[c(2,4)]),
+                   c(1000, yrange[c(1,3)]),
+                   c(1001, yrange[c(2,4)]),
+                   wrap = FALSE)
 
-animate(p2, device = "png", width = 1000, height = 600)
+gif2 <- animate(p2, device = "png", width = 1000, height = 600)
+gif2_split <- split_animation(gif2, cut(gganimate::frame_vars(gif2)$frame, c(0, 31, 70, 100)))
+gif2_split[[2]]
