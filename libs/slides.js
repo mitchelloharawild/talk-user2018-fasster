e84e0ce3e9ea1c91ddc363427e21fb05ad916d33
Mitchell <mitch.ohara-wild@monash.edu>
2018-07-13 01:41:56 +1000

    Fixed slides.js

--- libs/slides.js
+++ libs/slides.js
@@ -1,6 +1,8 @@
-slideshow.on('beforeShowSlide', function (slide) {
-  $("video").each(function() {
-    $(this).get(0).currentTime = 0;
-    $(this).get(0).play();
+ $(document).ready(function(){
+  slideshow.on('beforeShowSlide', function (slide) {
+    $("video").each(function() {
+      $(this).get(0).currentTime = 0;
+      $(this).get(0).play();
+    });
   });
-});
+ });
